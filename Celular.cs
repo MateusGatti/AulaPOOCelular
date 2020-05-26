9edb75270318a43f5d5a6c983e24ee1d6f7b638d
Mateus Mello <mt.gatti@hotmail.com>
2020-05-26 17:18:22 -0300

    projeto funcionando

---
+++ Celular.cs
@@ -0,0 +1,37 @@
+namespace AulaPOOCelular
+{
+    public class Celular
+    {
+        
+        public string cor;
+        public string tamanho;
+        public string modelo;
+        public bool ligado;
+        public string acao;
+        
+        public string VerAcao(){
+            return acao;
+        }
+        public string VerCor(){
+            return cor;
+        }
+        public string VerTamanho(){
+            return tamanho;
+        }
+        public string VerModelo(){
+            return modelo;
+        }
+        public bool VerLigado(){
+            return ligado;
+        }
+        
+        
+        
+
+
+
+        
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace AulaPOOCelular
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Celular iphone = new Celular();
+            iphone.cor = "Branco";
+            iphone.modelo = "Iphone X";
+            iphone.tamanho = "Médio";
+            iphone.ligado = true;
+            iphone.acao = "---";
+            
+
+            Console.WriteLine(iphone.VerModelo());
+            Console.WriteLine(iphone.VerCor ());
+            Console.WriteLine(iphone.VerTamanho());
+            Console.WriteLine(iphone.VerLigado());
+
+            if(iphone.ligado == false){
+                Console.WriteLine("Celular está desligado");
+            }else{
+                Console.WriteLine("O que deseja fazer? desligar, mensagem ou ligação?");
+            }
+            iphone.acao = Console.ReadLine();
+            if(iphone.acao == "desligar"){
+                Console.WriteLine("Celular Desligado");
+            }else if(iphone.acao == "mensagem"){
+                Console.WriteLine("Enviando Mensagem");
+            }else{
+                Console.WriteLine("Fazendo ligação");
+            }
+
+            
+            
+        }
+    }
+}
